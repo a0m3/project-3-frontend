bc852f9dc0b34bff362966db9fafed0b4d289538
a0m3 <almosawi0309@gmail.com>
2026-08-11 19:46:45 +0300

    profile page initial work

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useAuth } from '../context/AuthContext'
+import { getMyHistory } from '../services/historyService'
 
 function ProfilePage() {
+  const {user} = useAuth()
+
+  const [history, setHistory] = useState([])
+
+  useEffect(()=>{
+    async function loadHistory(){
+      try{
+        const data = await getMyHistory()
+        setHistory(data)
+      }
+      catch(err){
+        console.log(err)
+      }
+    }
+    loadHistory()
+  }, [])
+
+  let wins = 0
+  let best = 0
+
+    for (const game of history) {
+        if (game.status === "won") {
+            wins++;
+        }
+
+        if (game.moneyWon > best) {
+            best = game.moneyWon;
+        }
+    }
+    
   return (
     <div>ProfilePage</div>
   )
