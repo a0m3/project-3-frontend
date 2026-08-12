b6b33239b2e102fd997882cd742e4bddbcbcba29
a0m3 <almosawi0309@gmail.com>
2026-08-12 20:13:47 +0300

    dashboard classNames updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,23 +7,23 @@ function Dashboard() {
   const { user } = useAuth()
   return (
     <div className="page">
-      <div>
+      <div className="page-title">
         <h1>Welcome {user.username}</h1>
         <p>Choose a game mode</p>
       </div>
 
-      <div>
-        <Link to="/play/regular">
+      <div className="modes">
+        <Link className="mode" to="/play/regular">
         <h2>Regular Who wants to be a millionare game</h2>
         <p>Play the classic game</p>
         </Link>
 
-        <Link to="/custom-games">
+        <Link className="mode" to="/custom-games">
         <h2>Custom Games</h2>
         <p>Create and play your own questions</p>
         </Link>
 
-        <Link to="/history">
+        <Link className="mode" to="/history">
         <h2>Games History</h2>
         <p>Checkout your previous games status</p>
         </Link>
