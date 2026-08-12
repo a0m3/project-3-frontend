797534ec49cb115c814be3de0362fa4984d5abcc
a0m3 <almosawi0309@gmail.com>
2026-08-12 10:03:24 +0300

    regular game page initial work upload

--- src/pages/game/RegularGamePage.jsx
+++ src/pages/game/RegularGamePage.jsx
@@ -1,10 +1,48 @@
 import { useState } from "react"
 import { Link } from "react-router"
 import GamePlay from "../../components/GamePlay"
-import {getGameQuestions} from "../../services/questionService"
-import {moneyLadder, moneyAmount} from "../../components/MoneyLadder"
+import { getGameQuestions } from "../../services/questionService"
+import { moneyPool, moneyAmount } from "../../utils/moneyLadder"
+
 
 function RegularGamePage() {
+
+  const [game, setGame] = useState(null)
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(null)
+
+  async function startGame() {
+    setLoading(true)
+    setError('')
+
+    try{
+      const data = await getGameQuestions()
+      setGame(data)
+    }
+    catch(err){
+      setError(
+        error.response?.data?.message || 'Could not load questions.'
+      )
+    }
+    setLoading(false)
+
+  }
+
+if(game){
+  return(
+    <section className="page">
+      <GamePlay 
+      questions={game.questions}
+      ladder={game.ladder||moneyPool}
+      mode='regular'
+      gameName='Regular Millionare'
+      onPlayAgain={startGame}
+      ></GamePlay>
+    </section>
+  )
+}
+
+
   return (
     <div>RegularGamePage</div>
   )
