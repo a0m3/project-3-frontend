9d36a32589e20b99b26cc5ed43d744fd3bd2c355
a0m3 <almosawi0309@gmail.com>
2026-08-12 14:29:24 +0300

    game histories page initial work

--- src/pages/history/GameHistoryPage.jsx
+++ src/pages/history/GameHistoryPage.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { getMyHistory } from "../../services/historyService"
+import { moneyAmount } from "../../utils/moneyLadder"
+
+
 
 function GameHistoryPage() {
+
+  const [history, setHistory] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+    async function loadHistory() {
+      try {
+        const data = await getMyHistory()
+        setHistory(data)
+      }
+      catch (error) {
+        setError(
+          error.response?.data?.message ||
+          "Could not load history."
+        )
+      }
+      setLoading(false)
+    }
+    loadHistory()
+
+  }, [])
   return (
     <div>GameHistoryPage</div>
   )
