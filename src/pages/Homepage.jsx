c4ab65d0c70d1b32bfbbd1d0d43ddf1bb0de2da1
a0m3 <almosawi0309@gmail.com>
2026-08-12 19:58:22 +0300

    updated the className of divs in homepage for css

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Link } from 'react-router'
 import { useAuth } from '../context/AuthContext'
 
@@ -8,26 +7,26 @@ function Homepage() {
 
   return (
     <div className='page'>
-      <div>
+      <div className='hero'>
         <h1>Who Wants To Be A Millionare!</h1>
         <p>Answer questions, climb the money ladder, and try to win $1,000,000.</p>
 
         {!user ? (
-          <div className="btn-row">
-            <Link to="/sign-up" className="btn btn-primary">Sign Up Now to Play</Link>
-            <Link to="/sign-in" className="btn btn-secondary">Sign In</Link>
+          <div className="button-row">
+            <Link to="/sign-up" className="button main-button">Sign Up Now to Play</Link>
+            <Link to="/sign-in" className="button second-button">Sign In</Link>
           </div>
         ) : (
-          <Link to="/dashboard" className="btn btn-primary">Go to Game Modes</Link>
+          <Link to="/dashboard" className="button main-button">Go to Game Modes</Link>
         )}
       </div>
-      <div className="mode-grid">
-        <div className="mode-card">
+      <div className="modes">
+        <div className="mode">
           <h2>Regular Millionaire</h2>
           <p>Play the classic game with 15 levels.</p>
         </div>
 
-        <div className="mode-card">
+        <div className="mode">
           <h2>Custom Games</h2>
           <p>Create your own questions and play them.</p>
         </div>
